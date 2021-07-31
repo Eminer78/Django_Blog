17b2f8e80f15aa56a13cd18f05e418f9708ea6d3
Eric Miner <ericminer78@gmail.com>
2021-07-31 10:55:23 -0500

    changed STATIC_ROOT in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -124,7 +124,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT ='staticfiles'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -132,7 +133,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
