a9245ab7221c2fb503c6c3f3a26b4ca480f84a4b
Eric Miner <ericminer78@gmail.com>
2021-07-31 10:57:21 -0500

    reverted STATIC_ROOT in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -124,8 +124,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT ='staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
